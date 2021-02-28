ea9b29b16e54622e5fd9e77a626cc12172172500
rexhinald <rexhinald.cuke@nebula.al>
2021-02-28 20:03:25 +0100

    added map

--- src/views/map.js
+++ src/views/map.js
@@ -1,57 +1,34 @@
-import {useState} from 'react';
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const Map = () => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-
-    //POST api call to database
-    const createNewNote = () => {
-        //     axios.post('url', {
-        //         title: title,
-        //         description: description,
-        //     }).then((response) => {
-        //         logic after creating note
-        //     }).catch((error) => {
-        //         logic in case of error
-        //     })
-    }
-
-    return (
-        <div style={styleRoot}>
-            <h1>Create note</h1>
-            <button onClick={() => createNewNote()}>Create</button>
-            <input
-                style={styleTitle}
-                type={'text'}
-                placeholder={'Title'}
-                value={title}
-                onChange={(event) => setTitle(event.target.value)}
-            />
-            <textarea
-                rows={30}
-                style={styleDescription}
-                placeholder={'Description'}
-                value={description}
-                onChange={(event) => setDescription(event.target.value)}
-            />
-        </div>
-    )
+const styleMap = {
+    width: 800,
+    height: 600,
 }
-
 const styleRoot = {
     display: 'flex',
-    flexDirection: 'column',
-    padding: 10,
     justifyContent: 'center',
     alignItems: 'center',
 }
-const styleTitle = {
-    width: '50%',
-    margin: 10,
+
+const center = {
+    lat: 41.3275,
+    lng: 19.8187,
+};
+
+const Map =() => {
+    return (
+        <div style={styleRoot}>
+            <LoadScript
+                googleMapsApiKey="AIzaSyAy40HIQO-hI7SQWRg-HPUVjyEmGWtjLEI"
+            >
+                <GoogleMap
+                    mapContainerStyle={styleMap}
+                    center={center}
+                    zoom={13}
+                />
+            </LoadScript>
+        </div>
+    )
 }
-const styleDescription = {
-    width: '75%',
-    height: 400,
-    margin: 10,
-}
-export default Map;
+export default React.memo(Map);
